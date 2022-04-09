cce3e3d4550e9abec82b4009dced8de916b7fc3f
Sana Sabir <sanasabir6666@gmail.com>
2022-04-09 11:33:07 +0500

    themeData widget and ui of bmi calculator

---
+++ bmicalculator/lib/inputpage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('BMI Calculator'),
+      ),
+      body:Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),),
+              ),
+              Expanded(child: Container(margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),)),
+            ],
+          ),),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),),),
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),),),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),),
+            ],
+          ),),
+        ],
+      )
+    );
+  }
+}
---
+++ bmicalculator/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'inputpage.dart';
+void main(){
+  runApp(bmicalculator(),);
+}
+class bmicalculator extends StatelessWidget {
+  const bmicalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: homepage(),
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: Color(0xFF0A0E21),
+            primaryColor: Color(0xFF0A0E21),
+          ),
+    );
+  }
+}
+
+
