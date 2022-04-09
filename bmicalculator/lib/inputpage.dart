557923aa145c1804b8d3a2a7ba306b300cbb2b36
Sana Sabir <sanasabir6666@gmail.com>
2022-04-09 12:23:19 +0500

    refactor our code

--- bmicalculator/lib/inputpage.dart
+++ bmicalculator/lib/inputpage.dart
@@ -17,45 +17,44 @@ class _homepageState extends State<homepage> {
         children: [
           Expanded(child: Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),),
+              Expanded(child: Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
               ),
-              Expanded(child: Container(margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),)),
+              ),
+              Expanded(child:Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
+              ),
+              ),
             ],
           ),),
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(10.0),
-            ),),),
+          Expanded(child:Repeatcontainercode(
+            colors: Color(0xFF1D1E33),
+          ),),
           Expanded(child: Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),),
+              Expanded(child: Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
+              ),
+              ),
+              Expanded(child: Repeatcontainercode(  colors: Color(0xFF1D1E33),),),
             ],
           ),),
         ],
       )
     );
+  }
+}
+
+class Repeatcontainercode extends StatelessWidget {
+  Repeatcontainercode({@required  this.colors});
+  final Color? colors;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colors,
+        borderRadius: BorderRadius.circular(10.0),
+      ),);
   }
 }
