16ee73cada9bac31c374cf66ae68e2f2b351fd94
Sana Sabir <sanasabir6666@gmail.com>
2022-04-13 00:05:18 +0500

    add slider in bmi calculator

--- bmicalculator/lib/inputpage.dart
+++ bmicalculator/lib/inputpage.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'containercode.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'textoricon.dart';
-const activeColor = Color(0xFF1D1E33);
-const deActiveColor = Color(0xFF111328);
+import 'constantfile.dart';
 enum Gender{
   male,
   female,
 }
+int sliderheight=180;
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -24,7 +25,8 @@ class _InputPageState extends State<InputPage> {
       ),
       body:Column(
         children: [
-          Expanded(child: Row(
+          Expanded(
+            child: Row(
             children: [
               Expanded(
                 child: GestureDetector(
@@ -59,10 +61,40 @@ class _InputPageState extends State<InputPage> {
               ),
             ],
           ),),
-          Expanded(child:Repeatcontainercode(
-            colors: Color(0xFF1D1E33),
+          Expanded(
+            child:Repeatcontainercode(
+              colors: Color(0xFF1D1E33),
+              cardWidget: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                      'Height',
+                      style: kLabelStyle),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(sliderheight.toString(),style: knumberstyle),
+                      Text('cm',style: kLabelStyle),
+
+                    ],
+                  ),
+                  Slider(
+                    value: sliderheight.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newvalue){
+                      setState(() {
+                        sliderheight =newvalue.round();
+                      });
+                    },
+                  ),
+                ],
+              ),
           ),),
-          Expanded(child: Row(
+          Expanded(
+            child: Row(
             children: [
               Expanded(child: Repeatcontainercode(
                 colors: Color(0xFF1D1E33),
